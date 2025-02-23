2addc55fb377a27867671da59e0d1dd09fc87d7e
Floris de Graaff <florisdegraaff@gmail.com>
2025-02-23 14:41:28 +0100

    fix: sanity dataset not working as intended

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,14 +1,14 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'Learn',
 
-  projectId: 'tksen46a',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
+  dataset: process.env.SANITY_STUDIO_DATASET || "development",
 
   plugins: [structureTool(), visionTool()],
 
