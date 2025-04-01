fd14d72311aa6b9e0737d4a85b2577a43bcc595e
Floris de Graaff <florisdegraaff@gmail.com>
2025-04-01 18:44:02 +0200

    fix: images not working

--- apps/site/lib/sanity/urlFor.ts
+++ apps/site/lib/sanity/urlFor.ts
@@ -1,8 +1,8 @@
 import imageUrlBuilder from '@sanity/image-url'
 
 const builder = imageUrlBuilder({
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
-  dataset: process.env.SANITY_STUDIO_DATASET || '',
+  projectId: process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET || '',
 })
 
 export function urlFor(source: any) {
